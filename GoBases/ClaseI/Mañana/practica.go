34b1b5b191a3cef84361a643a79d5b442857cb0b
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-09-23 13:04:37 -0300

    todavia no agregue el modulo

---
+++ GoBases/ClaseI/Mañana/practica.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// ACTIVIDAD 1
+	nombresito := "FRANCO PESENDA"
+	direccion := "Altolaguirre"
+	fmt.Println("Nombre: " + nombresito)
+	fmt.Println("Direcciòn: " + direccion)
+	//ACTIVIDAD 2
+	temperatura := 25.3
+	humedad := 17.5
+	presion := 1025.5
+	fmt.Println(
+		"Temperatura: ", temperatura, "\n",
+		"Humedad: ", humedad, "\n",
+		"Presión: ", presion)
+	//ACTIVIDAD 3
+	var nombre string
+	var apellido string
+	var edad int
+	apellidoDos := 6
+	var licencoaDeComnducir = true
+	var estaturaDeLaPersona int
+	cantidadDeHijos := 2
+	//ACTIVIDAD 4
+	var apellidoA4 string = "Gomez"
+	var edad int = 35
+	boolean := "false"
+	var sueldo float64 = 45857.90
+	var nombre string = "Julian"
+}
