0975171763fa069792126ae0a9a981c1884af8d9
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-09-29 18:10:43 -0300

    nada funciona!!!!!

--- GoBases/ClaseV/maniana/ejercicio1/main.go
+++ GoBases/ClaseV/maniana/ejercicio1/main.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main() {
+	defer func() {
+		err := recover()
+
+		if err != nil {
+			fmt.Println("Ejecución finalizada")
+		}
+	}()
+
 	fmt.Println("Empezando...")
 	read, err := os.ReadFile("customers.txt")
 	if err != nil {
-		panic(err)
+		panic("aaaa")
 	}
 	data := string(read)
 	fmt.Println(data)
+	
 }
