9dbd49a1ab1b85eb93f627b0f050bd9a5ce5fd00
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-01 16:30:18 -0300

    ej 2 - tarde - clase 2 INCOMPLETO

--- GoBases/ClaseII/tarde/ejercicio2/main.go
+++ GoBases/ClaseII/tarde/ejercicio2/main.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
+type Matrix struct {
+	Elements	[]float64
+	Rows		int
+	Columns		int
+	Quadratic	bool
+	MaxValue	int
+}
+
+func (m *Matrix) Set(values ...float64) {
+	m.Elements = values
+}
+
+func (m Matrix) Print() {
+	fmt.Println(m.Elements)
+}
+
 func main() {
-	
+	m1 := Matrix{
+		Rows: 8,
+		Columns: 3,
+		Quadratic: false,
+		MaxValue: 3,
+	}
+
+	m1.Set(50000.00, 00.000010, 00.020, 10.00)
+
+	m1.Print()
 }
