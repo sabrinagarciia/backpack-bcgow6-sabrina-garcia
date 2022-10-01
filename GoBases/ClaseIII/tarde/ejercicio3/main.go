2590d530d7bab662892386e545f782aad3b967f0
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-01 19:19:35 -0300

    progreso ej3 tt clase3

--- GoBases/ClaseIII/tarde/ejercicio3/main.go
+++ GoBases/ClaseIII/tarde/ejercicio3/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+
+type Product struct {
+	Name		string
+	Price, Quantity	int
+}
+
+type Service struct {
+	Name		string
+	Price		float64
+	Minutes		int
+}
+
+type Maintenance struct {
+	Name		string
+	Price		float64
+}
+
+func totalProducts(products []Product) int {
+	sum := 0
+	for _, p := range products {
+		sum += p.Price * p.Quantity
+	}
+	return sum
+}
+
+func totalService(services []Service) {
 	
+}
+
+func finalTotal() {
+
+}
+
+func totalMaintenance() {
+	
+}
+
+func main() {
+
 }
