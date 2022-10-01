0b98dd034c88d1bc572d1a5119a5169c80574d91
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-01 18:23:17 -0300

    ej1 tm clase3

--- GoBases/ClaseIII/maniana/ejercicio1/main.go
+++ GoBases/ClaseIII/maniana/ejercicio1/main.go
@@ -1,9 +1,17 @@
 package main
 
-// import (
-// 	"os"
-// )
+import (
+	"log"
+	"os"
+)
 
 func main() {
+	data := []byte("1, $20, Alta\n2, $60, Baja\n")
 
+	//err := os.WriteFile("./info.csv", []byte("Id, Precio, Calidad\n"), 0666)
+	err := os.WriteFile("./info.csv", data, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
