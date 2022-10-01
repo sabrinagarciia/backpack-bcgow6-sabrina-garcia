9894013753649029976d7ce77f6dc25a08134cc0
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-01 19:07:22 -0300

    ej2 tt clase3

--- GoBases/ClaseIII/tarde/ejercicio2/main.go
+++ GoBases/ClaseIII/tarde/ejercicio2/main.go
@@ -1,1 +1,28 @@
 package main
+type User struct {
+	Name, Lastname, Email	string
+	Products				[]Product
+}
+
+type Product struct {
+	Name		string
+	Price		float64
+	Quantity	int
+}
+
+func newProduct(name *string, price *float64) *Product{
+	return &Product{Name: *name, Price: *price}
+}
+
+func (u *User)addProduct(product *Product, quantity *int) {
+	product.Quantity = *quantity
+	u.Products = append(u.Products, *product)
+}
+
+func (u *User) deleteProduct() {
+	u.Products = []Product{}
+}
+
+func main() {
+
+}
