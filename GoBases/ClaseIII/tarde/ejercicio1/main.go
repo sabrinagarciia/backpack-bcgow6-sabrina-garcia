62e780f382fbd8617475e8368d10340e8aaf6058
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-01 18:51:39 -0300

    ej1 tt clase3

--- GoBases/ClaseIII/tarde/ejercicio1/main.go
+++ GoBases/ClaseIII/tarde/ejercicio1/main.go
@@ -1,2 +1,27 @@
 package main
 
+type User struct {
+	Name, Lastname	string
+	Age				int
+	Email, Password	string
+}
+
+func (u *User) changeName(name *string) {
+	u.Name = *name
+}
+
+func (u *User) changeLastname(lastname *string) {
+	u.Lastname = *lastname
+}
+
+func (u *User) changeEmail(email *string) {
+	u.Email = *email
+}
+
+func (u *User) changePassword(pass *string) {
+	u.Password = *pass
+}
+
+func main() {
+
+}
