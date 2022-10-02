0953e952e18db32e12cce869b9b7a0fd6874e856
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-02 16:34:37 -0300

    ej2 tm clase4

--- GoBases/ClaseIV/maniana/ejercicio2/main.go
+++ GoBases/ClaseIV/maniana/ejercicio2/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+// type error interface {
+// 	Errors()	string
+// }
+
+type MyCustomError struct {
+	msg		error
+}
+
+// func (e *MyCustomError) Error() error {
+// 	return 
+// }
+
+func MyCustomErrorTest(salary int) (string, error) {
+	if salary < 1506000 {
+		return "", errors.New("error: el salario ingresado no alcanza el mínimo imponible")
+	}
+	return "Debe pagar impuesto", nil
+}
+
 func main() {
-	
+	salary := 300
+
+	fmt.Println(MyCustomErrorTest(salary))
 }
