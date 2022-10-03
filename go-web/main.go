7050ce767b978c01d6087c0ea6beb20332a8d996
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-10-03 13:23:04 -0300

    ej3 tm clase1 GetAll trae slice pero no del json

--- go-web/main.go
+++ go-web/main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
-	"fmt"
-	//"net/http"
-	"os"
+	//"fmt"
+	"net/http"
+	//"os"
 	"github.com/gin-gonic/gin"
 )
 
 type Product struct {
-	ID			int
-	Name		string
-	Color		string
-	Price		float64
-	Stock		int
-	Code		string
-	Published	bool
-	Date		string
+	ID			int `json:"id"`
+	Name		string `json:"name"`
+	Color		string `json:"color"`
+	Price		float64 `json:"price"`
+	Stock		int `json:"stock"`
+	Code		string `json:"code"`
+	Published	bool `json:"published"`
+	Date		string `json:"date"`
 }
 
-/*func GetAll(/*w http.ResponseWriter, req *http.Request) {
-	read, err := os.ReadFile("./products.json")
-	if err != nil {
-		fmt.Println(err)
-	}
+var products = []Product{
+    {
+        ID: 1,
+        Name: "lata",
+        Color: "blanco",
+        Price: 20.00,
+        Stock: 300,
+        Code: "a00a",
+        Published: false,
+        Date: "20-09-2022",
+    },
+    {
+        ID: 2,
+        Name: "bolsa",
+        Color: "gris",
+        Price: 3.00,
+        Stock: 20,
+        Code: "b00a",
+        Published: true,
+        Date: "20-09-2022",
+    },
+    {
+        ID: 3,
+        Name: "cajón",
+        Color: "marrón",
+        Price: 40.00,
+        Stock: 90,
+        Code: "c04j",
+        Published: false,
+        Date: "20-09-2022",
+    },
+}
 
-	data := string(read)
-	c.JSON(200, gin.H{"info": data})
-}*/
+func GetAll(c *gin.Context) {
+	c.JSON(http.StatusOK, products)
+}
 
 func main() {
 	// ------- Ejercicio 1 de la clase 1 -------
@@ -39,15 +66,15 @@ func main() {
 	})
 
 
-
-	router.GET("/products", func(ctx *gin.Context) {
-		read, err := os.ReadFile("./products.json")
-		if err != nil {
-			fmt.Println(err)
-		}
+	router.GET("/products", GetAll)
+	// router.GET("/products", func(ctx *gin.Context) {
+	// 	read, err := os.ReadFile("./products.json")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 	
-		data := string(read)
-		ctx.JSON(200, gin.H{"info": data})
-	})
+	// 	data := string(read)
+	// 	ctx.JSON(200, gin.H{"info": data})
+	// })
 	router.Run()
 }
